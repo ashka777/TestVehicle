086b83b140f7b543701325f6c8d00624ba8ababa
ashka777 <ashka777@ya.ru>
2021-07-30 17:42:36 +0200

    Подключены база, установил entity

---
+++ Connection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVehicle
+{
+    internal class Connection
+    {
+        internal static SqlConnection DBConnection()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection("server=DESKTOP-JFTOVAG;Trusted_Connection=Yes;DataBase=DBVehicle;");
+
+                if (sqlConnection.State == ConnectionState.Open)
+                    return sqlConnection;
+                else
+                    sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Ошибка: " + ex.Message);
+            }
+            //return new SqlConnection();
+
+
+            //    string dbPath = @"E:\Программирование\_С#\Test_Vehicle\DBVehicle.mdf";
+            //string connStr = @"server=.\DESKTOP-JFTOVAG;Trusted_Connection=Yes;DataBase=DBVehicle;";
+            //string connStr2 = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + dbPath;
+            //string connectionString = @"Data Source=E:\Программирование\_С#\Test_Vehicle\DBVehicle.mdf;Initial Catalog=DBVehicle;Integrated Security=True" +
+            //                            "providerName =System.Data.SqlClient";
+            //OleDbConnection connection = new OleDbConnection(connStr2);
+            //connection.Open();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestVehicle;
 
 namespace TestVehicle
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            // GetAllReport();
+            GetModel();
+        }
+        //private DataTable GetAllReport()
+        //{
+        //    string selectSQL = "Select * from dbo.tOrder" +
+        //        "INNER JOIN  where InsDate<='20170101'";
+
+
+        //    return;
+        //}
+
+        
+        public bool GetModel() // функция подключения к базе данных и обработка запросов
+        {
+            string selectSQL = "Select * from dbo.tModel";
+            DataTable dataTable = new DataTable("tModel");                // создаём таблицу в приложении
+            SqlConnection sqlConnection = Connection.DBConnection();                             // подключаемся к базе данных
+            using (SqlCommand sqlCommand = new SqlCommand(selectSQL, sqlConnection))
+            {
+                sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+                sqlDataAdapter.Fill(dataTable);
+
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    modelBox.Items.Add(dataRow);
+                    modelBox.SelectionBoxItem.
+                    modelBox.DataContext = dataRow;
+                }
+
+
+                if (sqlConnection.State == ConnectionState.Open)
+                    return true;
+                //};
+                return false;
+            }
+        }
+
+        private void ExcelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
