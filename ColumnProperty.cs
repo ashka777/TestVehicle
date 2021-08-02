64bd0bb048370bb8c7196909d2e3d6a66a8d049e
ashka777 <ashka777@ya.ru>
2021-08-02 23:44:11 +0200

    Не готово залика ячеек dataGrid

---
+++ ColumnProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVehicle
+{
+    class ColumnProperty
+    {
+        const float columnModel = 100;
+
+        public static List<string> GetColumnName()
+        {
+            List<string> columnNameProp = new List<string>();
+            columnNameProp.AddRange(new string[] { "ModelId", "Модель" });
+            for (int m = 0; m < 12; m++)
+            {
+                columnNameProp.Add(DateTime.MinValue.AddMonths(m).ToString("MMMM"));
+            }
+            return columnNameProp;
+        }
+
+        public static float[] GetColumnWidth(float dgWidth)
+        {
+            dgWidth -= columnModel; // размер datagrid минус столбец Модель
+            float[] columnWidthProp = new float[14];
+            columnWidthProp[1] = columnModel;
+            for (int m = 2; m < columnWidthProp.Count(); m++)
+            {
+                columnWidthProp[m] = dgWidth / 12;
+            }
+            return columnWidthProp;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,10 @@ namespace TestVehicle
 {
     public partial class MainWindow : Window
     {
+        float dgWidth;
         SqlConnection sqlConnection;
+        private int selectModelId = -1;
+        private string selectYear = DateTime.Now.Year.ToString();
 
         public MainWindow()
         {
@@ -29,114 +32,146 @@ namespace TestVehicle
             try
             {
                 sqlConnection = Connection.DBConnection(out stateConnLabel);
+                GetData();
             }
-            catch
+            catch (Exception ex)
             {
-                labelConnect.Content = "Подключение с БД отсутствует!";
-            }  
-            GetData();
+                labelConnect.Content = "Ошибка: " + ex.Message;
+            }
+
         }
 
         private void GetData()
         {
-
-            // GetAllReport();
-            GetModel();
-            GetTableGrid();
+            GetYearComboBox();
+            GetModelComboBox();
+            GetTableGrid(GetAllModels());
         }
-        private void GetTableGrid()
+
+        private DataTable GetAllModels()
         {
-            string selectSQL = "SELECT ord.Number, br.Name Brand, mod.Name Model, ve.Equipment Equip, vc.Color, ord.TotalSum Total, cus.Name " +
-                "FROM tOrder ord " +
-                "LEFT JOIN tPrice pr ON pr.Id = ord.Price " +
-                "LEFT JOIN tBrand br ON br.Id = ord.BrandId " +
-                "LEFT JOIN tModel mod ON mod.Id = ord.ModelId " +
-                "LEFT JOIN tVehicleEquipment ve ON ve.Id = pr.EquipmentId " +
-                "LEFT JOIN tVehicleColor vc ON vc.Id = pr.ColorId " +
-                "LEFT JOIN tCustomer cus ON cus.Id = ord.CustomerId";
-            var dgWidth = (int)dataGrid.Width;
-            //var dgHeight = dataGrid.Height;
+            string selectSQL = "WITH cte as (SELECT ord.ModelId, br.Name +' '+ mod.Name Model, ord.TotalSum Total, ord.InsDate Date " +
+                    "FROM tOrder ord " +
+                    "LEFT JOIN tPrice pr ON pr.Id = ord.Price " +
+                    "LEFT JOIN tBrand br ON br.Id = ord.BrandId " +
+                    "LEFT JOIN tModel mod ON mod.Id = ord.ModelId ";
+
+            if (selectYear != "Все")
+            {
+                selectSQL += string.Format("WHERE YEAR(ord.InsDate) = {0}) ", selectYear);
+            }
+            else
+                selectSQL += ") ";
+            selectSQL += "SELECT cte.ModelId, cte.Model, [Date]=MONTH(cte.Date), sum(Total) Total " +
+                "into #t1 FROM cte GROUP BY cte.ModelId, cte.Model, MONTH(cte.Date) " +
+                "select* from(select ModelId, Model, Total, [Date] as mon from #t1) as t " +
+                "pivot(sum(Total) for mon in ([1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12])) as result ";
+
+            if (selectModelId >= 0)
+            {
+                selectSQL += string.Format("WHERE ModelId = {0}", selectModelId);
+            }
+
             using (SqlCommand sqlCommand = new SqlCommand(selectSQL, sqlConnection))
             {
-                DataTable dataTable = new DataTable("tOrders");                // создаём таблицу в приложении
-                sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+                DataTable dataTable = new DataTable("tOrders");
+                sqlCommand.CommandText = selectSQL;
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
                 sqlDataAdapter.Fill(dataTable);
-                string[] columnNameProp = new string[] { "Номер", "Марка", "Модель", "Комплектация", "Цвет", "Стоимость", "Заказчик" };
-                int[] columnWidthProp = new int[] { 100, 100, 100, 120, 120, 120, 120 };
-                int i = 0;
-                foreach (DataColumn col in dataTable.Columns)
-                {
-                    dgWidth -= columnWidthProp[i];
-                    if (dgWidth < 100)
-                        columnWidthProp[i] += dgWidth;
-                    dataGrid.Columns.Add(
+                return dataTable;
+            }
+        }
+
+        private void GetTableGrid(DataTable dataTable)
+        {
+            int i = 0;
+            dgWidth = (int)dataGrid.Width;
+            List<string> columnNameProp = ColumnProperty.GetColumnName();
+            float[] columnWidthProp = ColumnProperty.GetColumnWidth(dgWidth);
+            foreach (DataColumn col in dataTable.Columns)
+            {
+                dgWidth -= columnWidthProp[i];
+                dataGrid.Columns.Add(
                       new DataGridTextColumn
                       {
                           Header = columnNameProp[i],
+                          Width = columnWidthProp[i],
                           Binding = new Binding(string.Format("{0}", col.ColumnName)),
-                          
-                    Width = columnWidthProp[i]
                       });
-                    //dgWidth -= columnWidthProp[i];
-                    i++;
-                }
-
-                dataGrid.DataContext = dataTable;
-                //DataGridTextColumn textColumn = new DataGridTextColumn();
-                //textColumn.Header = "Numbersss";
-                //textColumn.Binding = new Binding("Number");
-                dataGrid.ItemsSource = dataTable.DefaultView;
-                //dataGrid.Columns[0].Width = 100;
-                //dataGrid.Columns[0].Header = "Номер";
-                //dataGrid.Columns[1].Width = 100;
-                //dataGrid.Columns[1].Header = "Марка";
-                //dataGrid.Columns[2].Width = 100;
-                //dataGrid.Columns[2].Header = "Модель";
-                //dataGrid.Columns[3].Width = 120;
-                //dataGrid.Columns[3].Header = "Комплектация";
-                //dataGrid.Columns[4].Width = 120;
-                //dataGrid.Columns[4].Header = "Цвет";
-                //dataGrid.Columns[5].Width = 120;
-                //dataGrid.Columns[5].Header = "Цена";
-                //dataGrid.Columns[6].Width = 120;
-                //dataGrid.Columns[6].Header = "Заказчик";
-                labelConnect.Content = dataTable.Rows.Count;
+                i++;
             }
-               
+            dataGrid.Columns[0].Visibility = Visibility.Collapsed;
+            dataGrid.DataContext = dataTable;
+            dataGrid.ItemsSource = dataTable.DefaultView;
+
+            //for (int rows = 0; rows < dataGrid.Items.Count; rows++)
+            //{
+                foreach (var item in dataGrid.Columns)
+                {
+                    var s = item;
+                
+                for (int columns = 2; columns < dataGrid.Items.Count; columns++)
+                {
+                    if (dataGrid.Items[columns] is not null)
+                        if (Convert.ToInt32(dataGrid.Items[columns]) > 25000000)
+                            dataGrid.Items[columns] = System.Windows.Media.Brushes.LightYellow;
+                }
+            }
+            //}
+            labelConnect.Content = "Всего записей: " + dataTable.Rows.Count;
         }
 
 
-        public void GetModel() // функция подключения к базе данных и обработка запросов
+        public void GetModelComboBox()
         {
-            string selectSQL = "Select * from dbo.tModel";
-            DataTable dataTable = new DataTable("tModel");                // создаём таблицу в приложении
+            string selectSQL = "Select m.Id ModelId, b.Name +' '+ m.Name as Name from dbo.tBrand as b, dbo.tModel as m where b.Id = m.BrandId";
+            DataTable dataTableModel = new DataTable("tModel");                // создаём таблицу в приложении
             using (SqlCommand sqlCommand = new SqlCommand(selectSQL, sqlConnection))
             {
                 sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
                 SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
-                sqlDataAdapter.Fill(dataTable);
-                modelBox.ItemsSource = dataTable.DefaultView;
-                
-                //modelBox.DisplayMemberPath = "Name";
-                //modelBox.SelectedValuePath = "id";
-                //modelBox.Text = "Name";
-                //foreach (DataRow dataRow in dataTable.Rows)
-                //{
-                //    modelBox.Items.Add(dataRow);
-                //    //modelBox.DataContext = dataRow;
-                //}
+                sqlDataAdapter.Fill(dataTableModel);
+                dataTableModel.Rows.Add(new string[] { "-1", "Все" });
+                modelBox.ItemsSource = dataTableModel.DefaultView;
+                modelBox.SelectedIndex = modelBox.Items.Count - 1;
+            }
+        }
+
+        public void GetYearComboBox()
+        {
+            string selectSQL = "SELECT DISTINCT Convert(nvarchar,YEAR(InsDate)) [Date] FROM tOrder ORDER BY [Date]";
+            DataTable dataTableYear = new DataTable("tYear");                // создаём таблицу в приложении
+            using (SqlCommand sqlCommand = new SqlCommand(selectSQL, sqlConnection))
+            {
+                sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+                sqlDataAdapter.Fill(dataTableYear);
+                dataTableYear.Rows.Add("Все");
+                yearBox.ItemsSource = dataTableYear.DefaultView;
+                yearBox.SelectedIndex = yearBox.Items.Count-1;
             }
         }
 
         private void ExcelBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ExportExcel excel = new ExportExcel(GetAllModels());
         }
 
         private void modelBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var select = (ComboBoxItem)modelBox;
+            selectModelId = (int)modelBox.SelectedValue;
+            GetTableGrid(GetAllModels());
+        }
+
+        private void yearBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectYear = (string)yearBox.SelectedValue;
+            GetTableGrid(GetAllModels());
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Андрей.\n+79316055903.");
         }
     }
 }
